32cdd8f971ee23869ca9165f6329b150298a39e2
Chrysostomos Symvoulidis <simvoul@unipi.gr>
2023-06-26 18:20:47 +0300

    updates on graphs creation

--- src/main.py
+++ src/main.py
@@ -9,7 +9,8 @@ features = load_breast_cancer().feature_names
 dataset_title = load_breast_cancer().filename.replace(".csv", "").replace(".gz", "")
 
 class_used = True # Set True if class is used as feature, False otherwise
-no_d_sep = False # Set True to remove no d-separation edges, False otherwise
+no_d_sep = True # Set True to remove no d-separation edges, False otherwise
+remove_common_causes = False
 independence_test = "fisherz" # Set "fisherz" or "chi2"
 
 class_used_str = ""
@@ -34,6 +35,22 @@ if no_d_sep:
             edges.remove(edge)
     no_d_sep = True
     no_d_sep_str = "_no_d_separation"
+
+# Remove edges were one node is not an ancestor of the other
+for edge in edges:
+    if str(edge.get_endpoint1()) == "CIRCLE" or str(edge.get_endpoint2()) == "CIRCLE":
+        edges.remove(edge)
+
+for edge in edges:
+    if str(edge.get_endpoint1()) == "CIRCLE":
+        edges.remove(edge)
+
+# Remove edges were latent common cause exists 
+# (we need to do this step 4 times to remove all common causes for some reason)
+if remove_common_causes:
+    for edge in edges:
+        if str(edge.get_endpoint1()) == "ARROW" and str(edge.get_endpoint2()) == "ARROW":
+            edges.remove(edge)
 
 for node in nodes:
     node.set_name(features[int(node.get_name().replace("X", "")) - 1])
