f754a504207ae113ae2becbbb02fd3ee8da8508f
Chrysostomos Symvoulidis <simvoul@unipi.gr>
2023-07-21 17:02:02 +0300

    identified causal relations and created causal variables

--- main.py
+++ main.py
@@ -14,3 +14,9 @@ causal_graph = CausalGraph(initial_data_array=initial_data,
 causal_graph.create_graph()
 causal_graph.save_graph_as_figure()
 causal_graph.save_graph_as_dot()
+
+causal_graph.get_causal_relations()
+# causal_dataset = causal_graph.get_data_with_causal_variables()
+print(f"Initial DataFrame shape: {causal_graph.get_df().shape}")
+print(f"Causal DataFrame shape:  {causal_graph.get_causal_df().shape}")
+causal_graph.save_causal_df_as_csv()
